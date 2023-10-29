5e49b9f65ac94c881276b81b0abb30c9d21ed043
Egorbobkov <114438492+Egorbobkov@users.noreply.github.com>
2023-10-29 23:24:39 +0300

    add: october tasks

---
+++ prj.codeforces/1675b.cpp
@@ -0,0 +1,34 @@
+//
+// Created by egor on 29.10.2023.
+//
+#include <iostream>
+#include <vector>
+
+void solve(){
+    int t(0);
+    std::cin >> t;
+    std::vector<int>a(t);
+    for (auto& i : a) {
+        std::cin >> i;
+    }
+    int ans = 0;
+    for (int i = t - 2; i >= 0; i--) {
+        while (a[i] >= a[i + 1] && a[i] > 0) {
+            a[i] /= 2;
+            ans++;
+        }
+        if (a[i] == a[i + 1]) {
+            std::cout << -1 << '\n';
+            return;
+        }
+    }
+    std::cout << ans << '\n';
+}
+
+int main(){
+    int n(0);
+    std::cin >> n;
+
+    while (n--) solve();
+
+}
---
+++ prj.codeforces/339b.cpp
@@ -0,0 +1,20 @@
+//
+// Created by egor on 29.10.2023.
+//
+#include <iostream>
+
+int main()
+{
+    long long n, m, pos(1), res(0), temp;
+    std::cin >> n >> m;
+    for (int i = 0; i < m; ++i) {
+        std::cin >> temp;
+        if (temp < pos)
+            res += n - pos + temp;
+        else
+            res += temp - pos;
+        pos = temp;
+    }
+    std::cout << res << std::endl;
+    return 0;
+}
